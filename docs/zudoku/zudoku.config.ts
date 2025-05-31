5522e56dd66549cc065b8307d61175cd03e80d87
Vladislav <barshak.99.ua@gmail.com>
2025-05-31 15:24:44 +0300

    Init zudoku

---
+++ docs/zudoku/zudoku.config.ts
@@ -0,0 +1,31 @@
+import type {ZudokuConfig} from "zudoku";
+
+const config: ZudokuConfig = {
+    topNavigation: [
+        {id: "docs", label: "Documentation"},
+        {id: "api", label: "API Reference"},
+    ],
+    sidebar: {
+        docs: [
+            {
+                type: "category",
+                label: "Overview",
+                items: ["docs/installation", "docs/authorization", "docs/about"],
+            },
+        ],
+    },
+    redirects: [
+        {from: "/", to: "HotelBookingSystem//docs"},
+    ],
+
+    apis: {
+        type: "file",
+        input: "./apis/openapi.json",
+        navigationId: "api",
+    },
+    docs: {
+        files: "/pages/*/.{md,mdx}",
+    },
+};
+
+export default config;
